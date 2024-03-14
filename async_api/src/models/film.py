91330e16ed861f97aac3bd020e0b0072c1fb61ca
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-03-14 18:47:01 +0200

    fix for git tests

--- async_api/src/models/film.py
+++ async_api/src/models/film.py
@@ -1,14 +1,4 @@
-from typing import Any
-
-from collections.abc import Callable
-
-import orjson
-
 from pydantic import BaseModel
-
-
-def orjson_dumps(v: Any, *, default: Callable[[Any], Any]) -> bytes:
-    return orjson.dumps(v, default=default)
 
 
 class Film(BaseModel):
