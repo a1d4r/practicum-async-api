587ef297689baeb135f9e01af83b92a4a2885535
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-03-15 21:02:37 +0200

    black test

--- async_api/src/api/v1/films.py
+++ async_api/src/api/v1/films.py
@@ -12,9 +12,9 @@ class Film(BaseModel):
     title: str
 
 
-@router.get('/{film_id}', response_model=Film)
+@router.get("/{film_id}", response_model=Film)
 async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
     film = await film_service.get_by_id(film_id)
     if not film:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
     return Film(id=film.id, title=film.title)
