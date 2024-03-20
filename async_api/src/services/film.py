39e4acbe3cd672d9720a3329df29538b7a2f1439
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-03-20 17:59:16 +0200

    refactor

--- async_api/src/services/film.py
+++ async_api/src/services/film.py
@@ -5,14 +5,14 @@ from db.redis import get_redis
 from elasticsearch import AsyncElasticsearch, NotFoundError
 from fastapi import Depends
 from loguru import logger
+from typing import List, Type
+
 
 from models.film import FilmWork, FilmMinimal, FilmShort, FilmWorkMinimal
 from redis.asyncio import Redis
 from services.utils import get_key_by_args
 
-from elasticsearch_dsl import Search
-from elasticsearch_dsl.connections import connections
-
+from elasticsearch_dsl import Search, Document
 
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
@@ -24,13 +24,32 @@ class FilmService:
         self.redis = redis
         self.elastic = elastic
 
-    async def all_films_work(self, **kwargs) -> list[FilmWork]:
+    async def all_films(self, **kwargs) -> list[FilmWork]:
         films = await self._films_work_from_cache(**kwargs)
         if not films:
-            films = await self._get_film(**kwargs)
+            films = await self._get_films_from_elastic(FilmWork)
             if not films:
                 return []
-            await self._put_films_wo_to_cache(films, **kwargs)
+            await self._put_films_to_cache(FilmWork)
+        return films
+
+    def get_all_films_from_elasticsearch(
+        page_size=10, page=1, sort_by="title", genre=None, query=None
+    ):
+        films = Search(get_elastic())
+        if not films:
+            return
+
+        if genre:
+            films = films.filter("term", genre=genre)
+
+        if query:
+            films = films.query("match", title=query)
+
+        films = films.sort({sort_by: {"order": "asc"}})
+        response = films[(page - 1) * page_size : page * page_size].execute()
+        films = [hit.to_dict() for hit in response.hits]
+
         return films
 
     @staticmethod
@@ -53,42 +72,12 @@ class FilmService:
         pass
 
     async def get_film_by_id(self, film_id: str) -> FilmWork | None:
-        film = await self._film_from_cache(film_id)
+        film = await self._film_from_work_cache(film_id)
         if not film:
-            film = await self._get_film_from_elastic(film_id)
+            film = await self._get_film_from_elasticsearch(film_id)
             if not film:
                 return None
             await self._put_film_to_cache(film)
-
-        return film
-
-    async def get_film_minimal_by_id(self, film_id: str) -> FilmMinimal | None:
-        film = await self._film_minimal_from_cache(film_id)
-        if not film:
-            film = await self._get_film_minimal_from_elasticsearch(film_id)
-            if not film:
-                return None
-            await self._put_film_minimal_to_cache(film)
-
-        return film
-
-    async def get_film_short_by_id(self, film_id: str) -> FilmShort | None:
-        film = await self._film_short_from_cache(film_id)
-        if not film:
-            film = await self._get_film_short_from_elasticsearch(film_id)
-            if not film:
-                return None
-            await self._put_film_short_to_cache(film)
-
-        return film
-
-    async def get_film_work_minimal(self, film_id: str) -> FilmWorkMinimal | None:
-        film = await self._film_work_minimal_from_cache(film_id)
-        if not film:
-            film = await self._get_film_work_minimal_from_elasticsearch(film_id)
-            if not film:
-                return None
-            await self._put_film_work_minimal_to_cache(film)
 
         return film
 
@@ -99,7 +88,7 @@ class FilmService:
             return None
         return FilmWork(**doc["_source"])
 
-    async def _get_films_from_elastic(self, **kwargs) -> list[FilmWork]:
+    async def _get_films_from_elasticsearch(self, model: Type[Document]) -> List[Document]:
         try:
             films_work_search = Search(index="movies").using(get_elastic())
             films_work_response = films_work_search.execute()
@@ -107,58 +96,23 @@ class FilmService:
             return None
         return [hit for hit in films_work_response]
 
-    async def _get_film_minimal_from_elasticsearch(self, film_id: str) -> FilmMinimal | None:
-        try:
-            doc = await self.elastic.get(index="movies", id=film_id)
-        except NotFoundError:
-            return None
-        return FilmMinimal(**doc["_source"])
-
-    async def _get_film_short_from_elasticsearch(self, film_id: str) -> FilmShort | None:
-        try:
-            doc = await self.elastic.get(index="movies", id=film_id)
-        except NotFoundError:
-            return None
-        return FilmShort(**doc["_source"])
-
-    async def _get_film_work_minimal_from_elasticsearch(
-        self, film_id: str
-    ) -> FilmWorkMinimal | None:
-        try:
-            doc = await self.elastic.get(index="movies", id=film_id)
-        except NotFoundError:
-            return None
-        return FilmWorkMinimal(**doc["_source"])
-
-    async def _film_work_from_cache(self, film_id: str) -> FilmWork | None:
+    async def _film_from_cache(self, film_id: str) -> FilmWork | None:
         data = await self.redis.get(film_id)
         if not data:
             return None
 
         return FilmWork.parse_raw(data)
 
-    async def _film_minimal_from_cache(self, film_id: str) -> FilmMinimal | None:
+    async def _film_work_from_cache(self, film_id: str) -> FilmWork | None:
         data = await self.redis.get(film_id)
         if not data:
-            return None
+            film = await self._get_film_work_from_elastic_by_id(film_id)
+            if film:
+                await self.redis.set(film_id, film.json())
+            return film
+        return FilmWork.parse_raw(data)
 
-        return FilmMinimal.parse_raw(data)
-
-    async def _film_short_from_cache(self, film_id: str) -> FilmShort | None:
-        data = await self.redis.get(film_id)
-        if not data:
-            return None
-
-        return FilmShort.parse_raw(data)
-
-    async def _film_work_minimal_from_cache(self, film_id: str) -> FilmWorkMinimal | None:
-        data = await self.redis.get(film_id)
-        if not data:
-            return None
-
-        return FilmWorkMinimal.parse_raw(data)
-
-    async def _films_work_from_cache(self, **kwargs) -> list[FilmWork]:
+    async def _films_from_cache(self, **kwargs) -> list[FilmWork]:
         key = await get_key_by_args(**kwargs)
         data = await self.redis.get(key)
         if not data:
@@ -167,43 +121,7 @@ class FilmService:
 
         return [FilmWork.parse_raw(item) for item in data]
 
-    async def _films_minimal_from_cache(self, **kwargs) -> list[FilmMinimal]:
-        key = await get_key_by_args(**kwargs)
-        data = await self.redis.get(key)
-        if not data:
-            logger.debug("FilmMinimal was not found in the cache")
-            return None
-
-        return [FilmMinimal.parse_raw(item) for item in data]
-
-    async def _films_short_from_cache(self, **kwargs) -> list[FilmShort]:
-        key = await get_key_by_args(**kwargs)
-        data = await self.redis.get(key)
-        if not data:
-            logger.debug("FilmShort was not found in the cache")
-            return None
-
-        return [FilmShort.parse_raw(item) for item in data]
-
-    async def _films_work_minimal_from_cache(self, **kwargs) -> list[FilmWorkMinimal]:
-        key = await get_key_by_args(**kwargs)
-        data = await self.redis.get(key)
-        if not data:
-            logger.debug("FilmWorkMinimal was not found in the cache")
-            return None
-
-        return [FilmWorkMinimal.parse_raw(item) for item in data]
-
     async def _put_film_to_cache(self, film: FilmWork) -> None:
-        await self.redis.set(film.id, film, FILM_CACHE_EXPIRE_IN_SECONDS)
-
-    async def _put_film_minimal_to_cache(self, film: FilmWorkMinimal) -> None:
-        await self.redis.set(film.id, film, FILM_CACHE_EXPIRE_IN_SECONDS)
-
-    async def _put_film_short_to_cache(self, film: FilmShort) -> None:
-        await self.redis.set(film.id, film, FILM_CACHE_EXPIRE_IN_SECONDS)
-
-    async def _put_film_work_minimal_to_cache(self, film: FilmWorkMinimal) -> None:
         await self.redis.set(film.id, film, FILM_CACHE_EXPIRE_IN_SECONDS)
 
 
