dba51d4a906a3cb9d6778c311171e8f799f19a0f
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-03-21 20:07:30 +0200

    add elastic dict

--- async_api/src/services/utils.py
+++ async_api/src/services/utils.py
@@ -1,5 +1,15 @@
 from typing import Any
+from elasticsearch_dsl import Search, Q
+from db.elastic import get_elastic
 
 
 def get_key_by_args(*args: Any, **kwargs: Any) -> str:
     return f"{args}:{kwargs}"
+
+
+def get_elasticsearch_dict(index, query_params):
+    s = Search(using=get_elastic(), index=index)
+    query = Q("match", **query_params)
+    s = s.query(query)
+    response = s.execute()
+    return response.to_dict()
