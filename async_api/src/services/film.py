41553e17e03dff5bb7167c037f30b8fa62e01a0b
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-03-22 18:45:49 +0200

    films update after review

--- async_api/src/services/film.py
+++ async_api/src/services/film.py
@@ -1,15 +1,17 @@
-from typing import Any
+from typing import NewType
 
 from functools import lru_cache
 
 from db.elastic import get_elastic
 
 from elasticsearch import AsyncElasticsearch, NotFoundError
-from elasticsearch_dsl import Document, Search
+
 from fastapi import Depends
 from models.film import Film
+from uuid import UUID
 
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
+FilmID = NewType("FilmID", UUID)
 
 
 class FilmService:
@@ -18,7 +20,7 @@ class FilmService:
     def __init__(self, elastic: AsyncElasticsearch):
         self.elastic = elastic
 
-    async def all_films(self, **kwargs: dict[str, Any]) -> list[Film]:
+    async def films(self, **kwargs: dict[str, str]) -> list[Film]:
         films = await self._get_films_from_elasticsearch(**kwargs)
         if not films:
             return []
@@ -29,45 +31,73 @@ class FilmService:
         self,
         page_size: int = 10,
         page: int = 1,
-        sort_by: str = "title",
+        sort_order: str = "asc",
         genre: str | None = None,
         query: str | None = None,
     ) -> dict | None:
-        films = Search(using=get_elastic(), index="movies")
-        if not films:
+        films = []
+        if not self.elastic:
             return None
         if genre:
-            films = films.filter("term", genre=genre)
+            films = [
+                hit
+                for hit in self.elastic.search(
+                    index="movies",
+                    body={
+                        "query": {
+                            "bool": {
+                                "filter": [
+                                    {"term": {"genre": genre}},
+                                    {"match": {"title": query}},
+                                ]
+                            }
+                        },
+                        "sort": [{"{sort_by}": {sort_order}}],
+                        "from": (page - 1) * page_size,
+                        "size": page_size,
+                    },
+                ).hits
+            ]
+        else:
+            films = [
+                hit
+                for hit in self.elastic.search(
+                    index="movies",
+                    body={
+                        "query": {"match_all": {}},
+                        "sort": [{"{sort_by}": {sort_order}}],
+                        "from": (page - 1) * page_size,
+                        "size": page_size,
+                    },
+                ).hits
+            ]
 
-        if query:
-            films = films.query("match", title=query)
+        return [hit.to_dict() for hit in films]
 
-        films = films.sort({sort_by: {"order": "asc"}})
-        response = films[(page - 1) * page_size : page * page_size].execute()
-
-        return [hit.to_dict() for hit in response.hits]
-
-    @staticmethod
-    async def get_film_by_id(self, film_id: str) -> Film | None:
+    async def get_film_by_id(self, film_id: FilmID) -> Film | None:
         film = await self._get_film_from_elasticsearch(film_id)
         if not film:
             return None
 
         return film
 
-    async def _get_film_from_elasticsearch(self, film_id: str) -> Film:
+    async def _get_film_from_elasticsearch(self, film_id: FilmID) -> Film:
         try:
             doc = await self.elastic.get(index="movies", id=film_id)
         except NotFoundError:
             return None
         return Film.model_validate(doc["_source"])
 
-    async def _get_films_from_elasticsearch(self, model: Document) -> list[Document]:
-        films_search = Search(index="movies").using(get_elastic()).doc_type(model)
+    async def _get_films_from_elasticsearch(self, model: Film) -> list[Film]:
+        films_response = []
+        films_search = {"query": {"match_all": {}}}
         if not films_search:
             return None
-        films_response = films_search.execute()
-
+        try:
+            films_response = await self.es.search(index="movies", body=films_search, doc_type=model)
+        except Exception as e:
+            print(e)
+            return None
         return list(films_response)
 
 
