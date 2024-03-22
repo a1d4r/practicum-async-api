8bc8cf56e18ff5d567a90331ed716a5a7219f8cb
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-03-23 01:02:01 +0200

    add test

--- async_api/tests/test_services/test_film_service.py
+++ async_api/tests/test_services/test_film_service.py
@@ -2,6 +2,7 @@ from uuid import UUID
 
 import pytest
 
+from models.film import Film
 from services.film import FilmService
 
 
@@ -10,37 +11,36 @@ async def film_service(elastic):
     return FilmService(elastic)
 
 
-async def test_get_film_by_id(film_service: FilmService):
+async def test_search_film_by_id(film_service):
     # Arrange
-    python_wars_id = UUID("25515b8c-2580-11eb-adc1-0242ac120002")
+    star_wars_id = UUID("c35dc09c-8ace-46be-8941-7e50b768ec33")
 
     # Act
-    film = film_service.get_film_by_id(python_wars_id)
+    film = await film_service.get_by_id(star_wars_id)
 
     # Assert
     assert film is not None
-    assert film.id == python_wars_id
-    assert film.title == "Python Wars: Hidden API"
+    assert film.id == star_wars_id
+    assert film.title == "Star Wars"
+    assert film.imdb_rating == 6.6
+    assert (
+        film.description
+        == "Luke Skywalker, a young farmer from the desert planet of Tattooine, must save Princess Leia from the evil Darth Vader."
+    )
+    assert film.genre == ["Action", "Fantasy", "Adventure"]
+    assert film.actors == []
+    assert film.writers == [{"id": "a5a8f573-3cee-4ccc-8a2b-91cb9f55250a", "name": "George Lucas"}]
+    assert film.directors == []
 
 
-async def test_get_film_from_elasticsearch(film_service: FilmService):
+async def test_search_films(film_service):
     # Arrange
-    python_wars_id = UUID("25515b8c-2580-11eb-adc1-0242ac120002")
+    page = 2
+    size = 5
 
     # Act
-    film = film_service.get_film_elasticsearc(python_wars_id)
+    films = await film_service.search(page, size)
 
     # Assert
-    assert film is not None
-    assert film.id == python_wars_id
-    assert film.title == "Python Wars: Hidden API"
-
-
-async def test_get_films_from_elasticsearch(film_service: FilmService):
-    # Arrange
-    page = 1
-    size = 1
-
-    # Act
-    films = film_service.get_films_from_elasticsearch(page, size)
     assert len(films) == size
+    assert all(isinstance(film, Film) for film in films)
