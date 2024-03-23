fd6d00d00a4d9238f1d8e1468f2b7b7e1019121b
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-03-23 22:37:05 +0200

    try

--- async_api/src/models/person.py
+++ async_api/src/models/person.py
@@ -1,9 +1,19 @@
 from models.base import UUIDBase
 
 
+class PersonFilm(UUIDBase):
+    """Модель для хранения информации о фильме, в котором участвовал актёр."""
+
+    title: str
+    imdb_rating: float
+    roles: list[str]
+
+
 class PersonFilmRoles(UUIDBase):
     """Модель для хранения информации о ролях актёра в фильме."""
 
+    title: str
+    imdb_rating: float
     roles: list[str]
 
 
@@ -11,6 +21,13 @@ class Person(UUIDBase):
     """Модель для хранения информации об актёре."""
 
     full_name: str
+    films: list[PersonFilmRoles]
+
+
+class PersonService(UUIDBase):
+    full_name: str
+    imdb_rating: float
+
     films: list[PersonFilmRoles]
 
 
