de5b6728dd7241ec2f17303a7934f2c6b6435a6e
Aidar Garikhanov <a1d4r@yandex.ru>
2024-03-29 00:35:02 +0300

    Healthcheck for API

--- async_api/src/main.py
+++ async_api/src/main.py
@@ -6,6 +6,7 @@ from fastapi.responses import ORJSONResponse
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 
+from api import health
 from api.v1 import films, genres, persons
 from core.settings import settings
 from db.elastic import elasticsearch
@@ -28,6 +29,7 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
+app.include_router(health.router, tags=["Статус"])
 app.include_router(films.router, prefix="/v1/films", tags=["Фильмы"])
 app.include_router(persons.router, prefix="/v1/persons", tags=["Персоны"])
 app.include_router(genres.router, prefix="/v1/genres", tags=["Жанры"])
